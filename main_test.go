f731f1ece0446e73e672755f4f0ca5786ee3c3c0
user17359 <blazejryszard@gmail.com>
2024-04-22 01:19:00 +0200

    Added additional test cases

--- main_test.go
+++ main_test.go
@@ -33,3 +33,45 @@ func TestInvalidPath(t *testing.T) {
 		t.Errorf("got %t, wanted %t", got, want)
 	}
 }
+
+// TestIllegalFields uses file with added fields that are outside of provided description
+// while astrisk is still present in Resources field
+func TestIllegalFields(t *testing.T) {
+	got := Verify("resources/illegal_fields.json")
+	want := false
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+}
+
+// TestMultiplePolicies uses file with three policies, each with two having asterisk in Resource field,
+// and one having two Resources as a list
+func TestMultiplePolicies(t *testing.T) {
+	got := Verify("resources/multiple_policies.json")
+	want := false
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+}
+
+// TestMultiplePolicies uses file with three policies, none having asterisk in Resource field
+func TestMultiplePoliciesNoAsterisk(t *testing.T) {
+	got := Verify("resources/multiple_policies_no_asterisk.json")
+	want := true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+}
+
+// TestMultiplePolicies uses file with three policies, last one having asterisk in Resource field
+func TestMultiplePoliciesLastAsterisk(t *testing.T) {
+	got := Verify("resources/multiple_policies_last_asterisk.json")
+	want := true
+
+	if got != want {
+		t.Errorf("got %t, wanted %t", got, want)
+	}
+}
